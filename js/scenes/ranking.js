836455cbfbab5992fec00b977447ca2a9ac3c0a5
u1973244 <u1973244@campus.udg.edu>
2022-05-04 20:43:08 +0200

    update als rankings
    ara s'ordenen segons les parelles

--- js/scenes/ranking.js
+++ js/scenes/ranking.js
@@ -21,6 +21,15 @@ class RankingScene extends Phaser.Scene {
             var arrayRanking = JSON.parse(localStorage.ranking);
             console.log(arrayRanking[0]);
             if(!Array.isArray(arrayRanking)) arrayRanking = [];
+            for (let i=0; i<arrayRanking.length; i++){ //ordeno el array per punts, és l'algoritme menys eficient, però és el que recordo i suposo que és suficient
+                for (let j=i+1; j<arrayRanking.length; j++){
+                    if (arrayRanking[j].puntuacio>arrayRanking[i].puntuacio) {
+                        let partida=arrayRanking[i];
+                        arrayRanking[i]=arrayRanking[j];
+                        arrayRanking[j]=partida;
+                    }
+                }
+            }
             for (let i=0; i<arrayRanking.length; i++){
                 var text =this.add.text(280, i*30+50, (arrayRanking[i].username + ":     Nivell: " + arrayRanking[i].nivell + "   Parelles: " + arrayRanking[i].puntuacio), { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif', color: '#000000', fontSize: '20px' });
             }
